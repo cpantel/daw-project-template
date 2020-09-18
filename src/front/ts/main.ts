b15daf6feaa39cadcdf52f4537d1c6bacbc7935e
Carlos Pantelides <carlos.pantelides@gmail.com>
2020-09-17 21:56:47 -0300

    ejercicio 4

--- src/front/ts/main.ts
+++ src/front/ts/main.ts
@@ -6,12 +6,57 @@
  * Brief: Main frontend file (where the logic is)
 =============================================================================*/
 
+class User{
+    private _id:number;
+    private _name:string;
+    private _email:string;
+    private _isLogged:boolean;
+
+    constructor( id:number, name:string, email:string) {
+        this._id =id;
+        this._email= email;
+        this._name=name;
+    }
+    set name(name:string) {
+        this._name = name;
+    }
+    set email(email:string) {
+        this._email = email;
+    }
+    set id(id:number) {
+        this._id = id;
+    }
+
+    get id():number {
+        return this._id;
+    }
+
+    get name():string {
+        return this._name;
+    }
+
+    get email():string {
+        return this._email;
+    }
+
+    printInfo():void {
+        console.log("id: " +  this.id + " name: " + this.name + " email: " + this.email);
+    }
+
+
+}
+
 class Main {
   constructor(){
 
   }
   main():void {
-      console.log("mensaje Main.main")
+      let users:Array<User> = new Array<User>();
+      users.push(new User(0,"Ana", "ana@gmail.com"));
+      users.push(new User(1,"Beto", "beto@gmail.com"));
+      users.push(new User(2,"Camila", "camila@gmail.com"));
+      users.forEach( user => user.printInfo());
+
   }
 
 }
