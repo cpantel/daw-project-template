d4846e0a9985eb40c817da90f4dfda82409a269c
Carlos Pantelides <carlos.pantelides@gmail.com>
2020-09-24 19:53:26 -0300

    Ejercicio 4

---
+++ src/front/ts/MyFramework.ts
@@ -0,0 +1,7 @@
+class MyFramework {
+  getElementById(id:string):HTMLElement {
+    let e:HTMLElement;
+    e = document.getElementById(id);
+    return e;
+  }
+}
--- src/front/ts/main.ts
+++ src/front/ts/main.ts
@@ -1,51 +1,3 @@
-/*=============================================================================
- * Authors: Agustin Bassi, Brian Ducca, Santiago Germino 
- * Date: Jul 2020
- * Licence: GPLV3+
- * Project: DAW - CEIoT - Project Structure
- * Brief: Main frontend file (where the logic is)
-=============================================================================*/
-
-class User{
-    private _id:number;
-    private _name:string;
-    private _email:string;
-    private _isLogged:boolean;
-
-    constructor( id:number, name:string, email:string) {
-        this._id =id;
-        this._email= email;
-        this._name=name;
-    }
-    set name(name:string) {
-        this._name = name;
-    }
-    set email(email:string) {
-        this._email = email;
-    }
-    set id(id:number) {
-        this._id = id;
-    }
-
-    get id():number {
-        return this._id;
-    }
-
-    get name():string {
-        return this._name;
-    }
-
-    get email():string {
-        return this._email;
-    }
-
-    printInfo():void {
-        console.log("id: " +  this.id + " name: " + this.name + " email: " + this.email);
-    }
-
-
-}
-
 class Main {
   constructor(){
 
@@ -57,6 +9,11 @@ class Main {
       users.push(new User(2,"Camila", "camila@gmail.com"));
       users.forEach( user => user.printInfo());
 
+      let mf:MyFramework = new MyFramework();
+
+      let b:HTMLElement = mf.getElementById("button_1");
+
+      b.textContent = "Hello World";
   }
 
 }
